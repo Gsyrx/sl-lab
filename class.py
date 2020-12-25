3e36fa3b6e8b7f764afaf5726235e47176be906c
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-25 18:47:43 +0530

    simple class added

---
+++ class.py
@@ -0,0 +1,8 @@
+class Complex:
+  def __init__(self, realpart, imagpart):
+    self.realpart = realpart
+    self.imagpart = imagpart
+
+result = Complex(3.5,4.5)
+print("Real Part = " , result.realpart)
+print("Imaginary Part = ", result.imagpart)
