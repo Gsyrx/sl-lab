a557af94170ab2cf9378f3b054aaa2b901a2ece2
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-25 16:39:49 +0530

    some more list methods added

--- list.py
+++ list.py
@@ -8,4 +8,20 @@ print("\nNegative index :- " , a[-3])
 # Replaces subject at index 2
 a[2] = "English"
 print("\nNew values in the list:" , a)
+
+del a[2]
+print("\nNew values in the list:",a)
+
+#insert value at particular index
+a.insert(1,"Social studies")
+print("\nUpdated values in the list:",a)
+
+#reverse values
+a.reverse() 
+print("\nReversed values in the list:",a)
+
+#sorted values
+a.sort() 
+print("\nSorted values in the list:",a)
 print("\n")
+
