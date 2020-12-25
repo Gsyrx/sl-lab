65cba4a038c407d584521b2c6fa0781692a55e3f
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-25 16:56:58 +0530

    tuple and some build in functions

---
+++ tuples.py
@@ -0,0 +1,10 @@
+tuple1 = (10, 20, 30, 40, 50)
+tuple2 = ("a", "b", "c")
+tuple3 = tuple1 + tuple2
+
+print("Two tuples concatenated" , tuple3)
+
+#Built-in functions
+print("Maximum element" , max(tuple1))
+print("Minimum element" , min(tuple1))
+print("Length of tuple1 = " , len(tuple1))
