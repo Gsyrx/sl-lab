24923b065d3801b68dbe8ca75437b6fbb0d02c8c
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-25 16:07:16 +0530

    complete selection construct

---
+++ selection.py
@@ -0,0 +1,16 @@
+# Selection Construct
+
+first = input("Enter first number :- ")
+print("First number is :- ", first)
+
+second = input("Enter second number :- ")
+print("Second number is :- ", second)
+
+if first > second:
+  print("First number is greater than second number")
+
+elif second > first:
+  print("Second number is greater than first number")
+
+else:
+  print("Both numbers are equal")
