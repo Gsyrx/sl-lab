9976bf9187ebb05ce71d051133b60a63cfb55946
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-25 16:34:41 +0530

    complete list part

---
+++ list.py
@@ -0,0 +1,11 @@
+a = [ "Physics", "Mathematics", "Chemistry", "Biology" ]
+
+print("\nSet of subjects in the list:" , a[1:3])
+
+# indexing from the back side -1,-2,-3,...
+print("\nNegative index :- " , a[-3])
+
+# Replaces subject at index 2
+a[2] = "English"
+print("\nNew values in the list:" , a)
+print("\n")
