da6eaad195bcae42ac58a8ed4f9fcc81804930ba
Gaurav Kumar <gauravsyr10@gmail.com>
2021-01-01 18:10:08 +0530

    added question 10

---
+++ question-10/script.js
@@ -0,0 +1,19 @@
+let doller1 = parseInt(prompt('Enter amount in dollers : '));
+let rupee1 = doller1 * 70;
+document.writeln('Doller to Rupees : ' + rupee1);
+document.writeln();
+
+let rupees2 = parseInt(prompt('Enter amount in rupees : '));
+let doller2 = rupees2 / 70;
+document.writeln('Rupees to Dollers: ' + doller2);
+document.writeln();
+
+let rupee3 = parseInt(prompt('Enter amount in rupee3 : '));
+let euro1 = rupee3 / 90;
+document.writeln('Rupees to Euros: ' + euro1);
+document.writeln();
+
+let euro2 = parseInt(prompt('Enter amount in euros : '));
+let rupee4 = euro2 * 90;
+document.writeln('Euros to Rupees: ' + rupee4);
+document.writeln();
